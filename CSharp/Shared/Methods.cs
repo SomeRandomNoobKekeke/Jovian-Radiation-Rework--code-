7102bd1d1d053c101bd3597464790c57e6464fea
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2024-06-15 20:00:53 +0300

    monsters no longer spawn dead

--- CSharp/Shared/Methods.cs
+++ CSharp/Shared/Methods.cs
@@ -15,6 +15,19 @@ namespace BetterRadiation
 {
   public partial class Mod : IAssemblyPlugin
   {
+    public float HowIrradiatedIsCurrentLocation()
+    {
+      if (
+        GameMain.GameSession?.Map?.CurrentLocation == null ||
+        GameMain.GameSession.Map.Radiation == null ||
+        GameMain.GameSession.Map.Radiation.Enabled ||
+        GameMain.GameSession.Campaign == null
+      ) { return 0; }
+
+      return GameMain.GameSession.Map.CurrentLocation.MapPosition.X - GameMain.GameSession.Map.Radiation.Amount;
+    }
+
+
     public static bool MonsterEvent_InitEventSpecific_Replace(EventSet parentSet, MonsterEvent __instance)
     {
       MonsterEvent _ = __instance;
@@ -44,14 +57,14 @@ namespace BetterRadiation
           _.disallowed = true;
           continue;
         }
-        if (GameMain.GameSession.IsCurrentLocationRadiated())
-        {
-          AfflictionPrefab radiationPrefab = AfflictionPrefab.RadiationSickness;
-          Affliction affliction = new Affliction(radiationPrefab, radiationPrefab.MaxStrength);
-          createdCharacter?.CharacterHealth.ApplyAffliction(null, affliction);
-          // TODO test multiplayer
-          createdCharacter?.Kill(CauseOfDeathType.Affliction, affliction, log: false);
-        }
+        // if (GameMain.GameSession.IsCurrentLocationRadiated())
+        // {
+        //   AfflictionPrefab radiationPrefab = AfflictionPrefab.RadiationSickness;
+        //   Affliction affliction = new Affliction(radiationPrefab, radiationPrefab.MaxStrength);
+        //   createdCharacter?.CharacterHealth.ApplyAffliction(null, affliction);
+        //   // TODO test multiplayer
+        //   createdCharacter?.Kill(CauseOfDeathType.Affliction, affliction, log: false);
+        // }
         createdCharacter.DisabledByEvent = true;
         _.monsters.Add(createdCharacter);
       }
