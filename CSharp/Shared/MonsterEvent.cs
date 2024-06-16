c21d66f8550f9f8644a7858919c77e53a3ad605d
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2024-06-16 16:27:41 +0300

    +Radiation_OnStep_Replace Radiation_UpdateRadiation_Replace

---
+++ CSharp/Shared/MonsterEvent.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Barotrauma;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+
+
+
+namespace BetterRadiation
+{
+  public partial class Mod : IAssemblyPlugin
+  {
+
+    public static bool MonsterEvent_InitEventSpecific_Replace(EventSet parentSet, MonsterEvent __instance)
+    {
+      MonsterEvent _ = __instance;
+
+      if (parentSet != null && _.resetTime == 0)
+      {
+        // Use the parent reset time only if there's no reset time defined for the event.
+        _.resetTime = parentSet.ResetTime;
+      }
+      if (GameSettings.CurrentConfig.VerboseLogging)
+      {
+        DebugConsole.NewMessage("Initialized MonsterEvent (" + _.SpeciesName + ")", Color.White);
+      }
+
+      _.monsters.Clear();
+
+      //+1 because Range returns an integer less than the max value
+      int amount = Rand.Range(_.MinAmount, _.MaxAmount + 1);
+      for (int i = 0; i < amount; i++)
+      {
+        string seed = i.ToString() + Level.Loaded.Seed;
+        Character createdCharacter = Character.Create(_.SpeciesName, Vector2.Zero, seed, characterInfo: null, isRemotePlayer: false, hasAi: true, createNetworkEvent: true, throwErrorIfNotFound: false);
+        if (createdCharacter == null)
+        {
+          DebugConsole.AddWarning($"Error in MonsterEvent: failed to spawn the character \"{_.SpeciesName}\". Content package: \"{_.prefab.ConfigElement?.ContentPackage?.Name ?? "unknown"}\".",
+              _.Prefab.ContentPackage);
+          _.disallowed = true;
+          continue;
+        }
+        // if (GameMain.GameSession.IsCurrentLocationRadiated())
+        // {
+        //   AfflictionPrefab radiationPrefab = AfflictionPrefab.RadiationSickness;
+        //   Affliction affliction = new Affliction(radiationPrefab, radiationPrefab.MaxStrength);
+        //   createdCharacter?.CharacterHealth.ApplyAffliction(null, affliction);
+        //   // TODO test multiplayer
+        //   createdCharacter?.Kill(CauseOfDeathType.Affliction, affliction, log: false);
+        // }
+        createdCharacter.DisabledByEvent = true;
+        _.monsters.Add(createdCharacter);
+      }
+
+      return false;
+    }
+
+
+  }
+}
---
+++ CSharp/Shared/Radiation.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Barotrauma;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+
+using Barotrauma.Extensions;
+
+
+
+namespace BetterRadiation
+{
+  public partial class Mod : IAssemblyPlugin
+  {
+    public static bool Radiation_OnStep_Replace(float steps, Radiation __instance)
+    {
+      Radiation _ = __instance;
+
+      if (!_.Enabled) { return false; }
+      if (steps <= 0) { return false; }
+
+      float increaseAmount = _.Params.RadiationStep * steps;
+
+      if (_.Params.MaxRadiation > 0 && _.Params.MaxRadiation < _.Amount + increaseAmount)
+      {
+        increaseAmount = _.Params.MaxRadiation - _.Amount;
+      }
+
+      _.IncreaseRadiation(increaseAmount);
+
+      int amountOfOutposts = _.Map.Locations.Count(location => location.Type.HasOutpost && !location.IsCriticallyRadiated());
+
+      foreach (Location location in _.Map.Locations.Where(_.Contains))
+      {
+        if (location.IsGateBetweenBiomes)
+        {
+          location.Connections.ForEach(c => c.Locked = false);
+          continue;
+        }
+
+        if (amountOfOutposts <= _.Params.MinimumOutpostAmount) { break; }
+
+        if (_.Map.CurrentLocation is { } currLocation)
+        {
+          // Don't advance on nearby locations to avoid buggy behavior
+          if (currLocation == location || currLocation.Connections.Any(lc => lc.OtherLocation(currLocation) == location)) { continue; }
+        }
+
+        bool wasCritical = location.IsCriticallyRadiated();
+
+        location.TurnsInRadiation++;
+
+        if (location.Type.HasOutpost && !wasCritical && location.IsCriticallyRadiated())
+        {
+          location.ClearMissions();
+          amountOfOutposts--;
+        }
+      }
+
+      return false;
+    }
+
+    public static bool Radiation_UpdateRadiation_Replace(float deltaTime, Radiation __instance)
+    {
+      Radiation _ = __instance;
+
+      if (!(GameMain.GameSession?.IsCurrentLocationRadiated() ?? false)) { return false; }
+
+      if (GameMain.NetworkMember is { IsClient: true }) { return false; }
+
+      if (_.radiationTimer > 0)
+      {
+        _.radiationTimer -= deltaTime;
+        return false;
+      }
+
+      if (_.radiationAffliction == null)
+      {
+        float radiationStrengthChange = AfflictionPrefab.RadiationSickness.Effects.FirstOrDefault()?.StrengthChange ?? 0.0f;
+        _.radiationAffliction = new Affliction(
+            AfflictionPrefab.RadiationSickness,
+            (_.Params.RadiationDamageAmount - radiationStrengthChange) * _.Params.RadiationDamageDelay);
+      }
+
+      _.radiationTimer = _.Params.RadiationDamageDelay;
+
+      foreach (Character character in Character.CharacterList)
+      {
+        if (character.IsDead || character.Removed || !(character.CharacterHealth is { } health)) { continue; }
+
+        if (_.IsEntityRadiated(character))
+        {
+          var limb = character.AnimController.MainLimb;
+          AttackResult attackResult = limb.AddDamage(limb.SimPosition, _.radiationAffliction.ToEnumerable(), playSound: false);
+          character.CharacterHealth.ApplyDamage(limb, attackResult);
+        }
+      }
+
+      return false;
+    }
+
+  }
+}
