c454773daa556c21ef3a27c959fcef26367dcf03
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2024-06-17 20:19:20 +0300

    1.1.1

--- CSharp/Shared/Mod.cs
+++ CSharp/Shared/Mod.cs
@@ -8,7 +8,11 @@ using System.Linq;
 using Barotrauma;
 using HarmonyLib;
 using Microsoft.Xna.Framework;
+
+#if CLIENT
 using Microsoft.Xna.Framework.Graphics;
+#endif
+
 
 using Barotrauma.Extensions;
 namespace BetterRadiation
@@ -54,13 +58,15 @@ namespace BetterRadiation
       init();
     }
 
+#if CLIENT
     public static void Level_DrawBack_Postfix(GraphicsDevice graphics, SpriteBatch spriteBatch, Camera cam, Level __instance)
     {
       float time = (float)(Timing.TotalTime / 100.0f);
 
-      float rad = Math.Clamp(CameraIrradiation(cam) * RadiationToColor - PerlinNoise.GetPerlin(time, time * 0.5f) * 0.3f, 0, 0.4f);
+      float rad = Math.Clamp(CameraIrradiation(cam) * RadiationToColor - PerlinNoise.GetPerlin(time, time * 0.5f) * 0.3f, 0, 0.38f);
       GameMain.LightManager.AmbientLight = GameMain.LightManager.AmbientLight.Add(new Color(0, rad, rad));
     }
+#endif
 
     public void patchAll()
     {
