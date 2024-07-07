6451f435ae20aa25b22e9d04424141d3bec6c684
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2024-07-07 16:00:01 +0300

    progress on save quit

--- CSharp/Shared/Patches/CampaignMode.cs
+++ CSharp/Shared/Patches/CampaignMode.cs
@@ -15,7 +15,21 @@ namespace JovianRadiationRework
   {
     public static bool CampaignMode_HandleSaveAndQuit_Prefix(CampaignMode __instance)
     {
-      log("lol you quited!");
+      float roundDuration = GameMain.GameSession?.RoundDuration ?? 0.0f;
+
+      float radSteps = roundDuration / (60.0f * settings.modSettings.Progress.WorldProgressStepDuration);
+
+      radSteps = Math.Max(0, Math.Min(radSteps, settings.modSettings.Progress.WorldProgressMaxStepsPerRound));
+
+      if (radSteps < settings.modSettings.Progress.GracePeriod) radSteps = 0;
+
+      if (!settings.modSettings.Progress.SmoothProgress)
+      {
+        radSteps = (float)Math.Floor(radSteps);
+      }
+
+      _.Radiation?.OnStep(radSteps);
+
       return true;
     }
   }
