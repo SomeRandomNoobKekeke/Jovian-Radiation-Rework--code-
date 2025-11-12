aad4b2b90857a37ad26692a08e5ed2522b8bc849
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2025-11-12 06:32:20 +0300

    moved AmbientLight settings to shared

---
+++ CSharp/Shared/Models/Modded/AmbientLight/Model.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Reflection;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Linq;
+
+using Barotrauma;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+
+
+namespace JovianRadiationRework
+{
+  public partial class AmbientLightModel : RadiationModel
+  {
+    public partial class ModelSettings : IConfig
+    {
+      public float RadiationToAmbienceBrightness { get; set; } = 0.02f;
+      public float MaxAmbienceBrightness { get; set; } = 2.0f;
+      public float AmbienceNoiseAmplitude { get; set; } = 0.5f;
+      public Color ActualColor { get; set; } = new Color(0, 0, 255);
+      public float PerlinNoiseFrequency { get; set; } = 0.04f;
+      public float RadiationAmountOffset { get; set; } = 100.0f;
+    }
+  }
+
+
+
+}
